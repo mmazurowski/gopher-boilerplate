bc60819408ac3f79596730877f316f1f511a9836
Marcin Mazurowski <34674660+mmazurowski@users.noreply.github.com>
2022-04-27 17:56:06 +0200

    feat: event bus setup

--- src/modules/hello-say-hello/framework/main.go
+++ src/modules/hello-say-hello/framework/main.go
@@ -1,9 +1,12 @@
 package framework
 
 import (
+	"context"
+
 	"github.com/mmazurowski/gopher-boilerplate/src/framework/cqrs"
 	"github.com/mmazurowski/gopher-boilerplate/src/framework/logger"
 	"github.com/mmazurowski/gopher-boilerplate/src/modules/hello-say-hello/app/commands"
+	eventbus "github.com/mmazurowski/gopher-boilerplate/src/shared/event-bus"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -12,12 +15,18 @@ type Application struct {
 	Logger *log.Logger
 }
 
-func CreateApplication() *Application {
+func CreateApplication(ctx context.Context) (app *Application, err error) {
 	logInstance := logger.Factory()
-	handlers := []cqrs.Handler{commands.NewSayHelloCommandHandler(logInstance)}
+	eb, err := eventbus.New(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	handlers := []cqrs.Handler{commands.NewSayHelloCommandHandler(logInstance, eb)}
 
 	return &Application{
 		Bus:    cqrs.Factory(handlers),
 		Logger: logInstance,
-	}
+	}, nil
 }
---
+++ src/shared/event-bus/event-bus.go
@@ -0,0 +1,58 @@
+package event_bus
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchevents"
+	"github.com/aws/aws-sdk-go-v2/service/cloudwatchevents/types"
+	"github.com/mmazurowski/gopher-boilerplate/src/shared/aws"
+)
+
+type EventBus struct {
+	busName         string
+	applicationName string
+	client          *cloudwatchevents.Client
+}
+
+func New(ctx context.Context) (*EventBus, error) {
+	client, err := aws.CreateEventBridgeClient(ctx)
+
+	if err != nil {
+		return nil, err
+	}
+
+	busName := os.Getenv("ENTERPRISE_EVENT_BRIDGE")
+	applicationName := os.Getenv("SERVICE_NAME")
+
+	if len(busName) == 0 {
+		return nil, errors.New("ENTERPRISE_EVENT_BRIDGE env variable must be present")
+	}
+
+	if len(applicationName) == 0 {
+		return nil, errors.New("SERVICE_NAME env variable must be present")
+	}
+
+	return &EventBus{busName: busName, applicationName: applicationName, client: client}, nil
+}
+
+func (bus *EventBus) Publish(ctx context.Context, messages []EventMessage) error {
+	entries := make([]types.PutEventsRequestEntry, len(messages))
+
+	for i := range messages {
+		message := messages[i]
+
+		entries[i] = types.PutEventsRequestEntry{DetailType: &message.detailType, EventBusName: &bus.busName, Source: &bus.applicationName, Time: &message.time, Detail: &message.message}
+	}
+
+	input := cloudwatchevents.PutEventsInput{Entries: entries}
+
+	_, err := bus.client.PutEvents(ctx, &input)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
